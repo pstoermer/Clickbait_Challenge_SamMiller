4fb97b4356a75ba70deb891c7718f30fa0fafaed
EstrixDS <49279931+EstrixDS@users.noreply.github.com>
2023-01-27 01:51:33 +0100

    deleted code, which led to infinite amounts of same labels

--- xlnettest.py
+++ xlnettest.py
@@ -3,7 +3,7 @@ import argparse
 import json
 import pandas as pd
 import numpy as np
-from transformers import AutoModelForSequenceClassification, XLNetTokenizer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 from keras.utils import pad_sequences
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
@@ -20,12 +20,7 @@ def parse_args():
 def load_input(df):
     if type(df) != pd.DataFrame:
         df = pd.read_json(df, lines=True)
-    
-    ret = []
-    for _, i in df.iterrows():
-        ret += [{'text': ' '.join(i['postText']) + ' - ' + i['targetTitle'] + ' ' + ' '.join(i['targetParagraphs']), 'uuid': i['uuid']}]
-    
-    return pd.DataFrame(ret)
+    return pd.DataFrame(df)
 
 
 def use_cuda():
@@ -34,23 +29,18 @@ def use_cuda():
 
 def predict(df):
     df = load_input(df)
-    uuids = list(df['uuid'])
-    decoding_array = {  
-                1: "passage",
-                2 : "phrase",
-                0 : "multi"
-                }
-    model = AutoModelForSequenceClassification.from_pretrained("EstrixDS/XLNet_SemEval_Task1")
-    tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased', do_lower_case=True)
+    model= AutoModelForSequenceClassification.from_pretrained("EstrixDS/XLNet_SemEval_Task1")
+    tokenizer = AutoTokenizer.from_pretrained("xlnet-base-cased")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Create sentence and label lists
-    sentences = df['text'].values
+    sentences = df.postText.values
 
     # We need to add special tokens at the beginning and end of each sentence for XLNet to work properly
     sentences = [sentence[0] + " [SEP] [CLS]" for sentence in sentences]
     tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
 
-
-    MAX_LEN = 256
+    uuids = list(df['uuid'])
+    MAX_LEN = 128
     # Use the XLNet tokenizer to convert the tokens to their index numbers in the XLNet vocabulary
     input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
     # Pad our input tokens
@@ -78,12 +68,12 @@ def predict(df):
     model.eval()
 
     # Tracking variables 
-    predictions = []
-
+    predictions= []
+    decoded_pred = []
     # Predict 
     for batch in prediction_dataloader:
         # Add batch to GPU
-        batch = tuple(t for t in batch)
+        batch = tuple(t.to(device) for t in batch)
         # Unpack the inputs from our dataloader
         b_input_ids, b_input_mask = batch
         # Telling the model not to compute or store gradients, saving memory and speeding up prediction
@@ -98,14 +88,17 @@ def predict(df):
         # Store predictions and true labels
         for i in logits:
             predictions.append(i)
-    decoded_pred = []
-    for z in predictions:
-        label = np.where(z == z.max())
-        decoded_label = decoding_array[label[0][0]]
-        decoded_pred.append(decoded_label)
-    for i,z in zip(uuids,decoded_pred):
-        yield {'uuid': i, 'spoilerType': z}
-
+        decoding_array = {  
+                1: "passage",
+                2 : "phrase",
+                0 : "multi"
+                }
+        for z in predictions:
+                label = np.where(z == z.min())[0][0]
+                decoded_label = decoding_array[label]
+                decoded_pred.append(decoded_label)
+        for i,z in zip(uuids,decoded_pred):
+            yield {'uuid': i, 'spoilerType': z}
 
 def run_baseline(input_file, output_file):
     with open(output_file, 'w') as out:
